22a55b4972a509d7da70e0c93152858f13e509be
Tony Ganesh Prajapati <133465084+gittonyp@users.noreply.github.com>
2024-08-20 05:09:51 +0000

    dfs

---
+++ abc.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([5, 1, 4,1,6,4,9])
+b= np.array([5, 1,6,4,78,475, 4,4])
+print(np.add(a,b))
+
+print(np.sort(a))
---
+++ mul.py
@@ -0,0 +1,6 @@
+import numpy as np
+a=np.array([[5, 1, 2],
+                           [4, 5, 6]])
+b=np.array([[3,3,2,5,1],[3,1,1,1,1],[1,1,2,2,2]])
+
+print(np.matmul(a,b))
